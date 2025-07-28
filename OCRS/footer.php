c7d525e8c062016ff10b7d7ee44d3224819f4613
Heng50 <144310456+Heng50@users.noreply.github.com>
2025-07-29 07:54:04 +0800

    Delete OCRS directory

--- OCRS/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer class="bg-black text-white text-center py-4 mt-auto">
-        <p class="text-sm">Copyright &copy; 2025 Online Course Registration System</p>
-    </footer>
-</body>
-</html>
